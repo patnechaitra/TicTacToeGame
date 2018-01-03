c35f6288530d192a51165fc3fc14f4fbb898c93a
admin <admin@192.168.1.7>
2018-01-03 23:30:38 +0530

    first commit

---
+++ TicTacToeGame/src/com/chaitrali/tictactoegame/model/Player.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chaitrali.tictactoegame.model;
+
+import java.awt.Color;
+/**
+ *
+ * @author chaitraliP
+ */
+public class Player extends Players {
+    
+    public Player(String name) {
+        super(name);
+        this.playing = false;
+        this.type = "X";
+        this.color = Color.green.darker();
+    }
+    
+    @Override
+    public void pressButton(String[][] table, String guest, int i, int j) {
+        // Check if the places is out of bound
+        if (i < 3 && j < 3) {
+            // Check if the place given weren't used yet
+            if (!table[i][j].equals("X") && !table[i][j].equals("O")) {
+                table[i][j] = guest;
+                this.playing = false;
+            } else {
+                System.out.println("Error: YOU CANNOT GUEST HERE!");
+            }
+        } else {
+            System.out.println("Error: PLACES GIVEN OUT OF BOUND!");
+        }
+    }
+}
---
+++ TicTacToeGame/src/com/chaitrali/tictactoegame/test/MainTest.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chaitrali.tictactoegame.test;
+
+import java.util.Scanner;
+
+import com.chaitrali.tictactoegame.controller.GameController;
+import com.chaitrali.tictactoegame.model.Player;
+
+/**
+ *
+ * @author chaitraliP
+ */
+public class MainTest {
+
+    public static String table;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Player player = new Player("Player");
+        GameController game = new GameController(player);
+        
+
+        while (game.isRunning()) {
+            //game.update();
+            // this command is just for test
+            drawBoard(game.getTable());
+            
+            
+            
+            // Test section
+            try {
+                if (game.isRunning()) {
+
+                    System.out.print("Give a ROW (between 1 and 3): $ ");
+                    int rowGuest = input.nextInt() - 1;
+                    System.out.print("Give a COL (between 1 and 3): $ ");
+                    int columnGuest = input.nextInt() - 1;
+
+                    System.out.println("\n\n\n************************************");
+                    player.pressButton(game.getTable(),"X", rowGuest, columnGuest);
+                }
+            } catch (Exception e) {
+                game.setRunning(false);
+                System.out.println("Error: " + e);
+            }
+
+        }
+
+    }
+
+    // Just for test
+    static void drawBoard(String[][] places) {
+
+        // Prints column position ex: # 1 2 3
+        table = "# ";
+        for (int i = 0; i < places.length; i++) {
+            table += "  " + (i + 1) + "  ";
+        }
+        table += ("\n");
+        /**
+         * prints row position and game status ex: a [X][O][O] b [O][O][X] c
+         * [X][X][O]
+         */
+        for (int i = 0; i < places.length; i++) {
+
+            table += (i + 1) + " ";
+            for (int j = 0; j < places.length; j++) {
+                table += "[ ";
+                table += places[i][j];
+                table += " ]";
+            }
+            table += "\n";
+
+        }
+        System.out.println(table);
+    }
+
+}
